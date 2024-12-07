7d916dca5ed78eb2d0073c1d7613cf2225ac7431
kayra <157609274+ibrahimKayraDural@users.noreply.github.com>
2024-12-07 16:06:29 +0300

    Test Script and object

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    [SerializeField] GameObject _Cube;
+    [SerializeField] TextMeshProUGUI _Tmesh;
+    [SerializeField] Image _Img;
+    [SerializeField] bool _ShowDebugLogs;
+    void Awake()
+    {
+        WriteDebug("Awake ran");
+    }
+    void Start()
+    {
+        WriteDebug("Start ran");
+    }
+    void Update()
+    {
+        #region Input
+        if (Input.GetKeyDown(KeyCode.P)) P_WasPressed();
+        else if (Input.GetKeyDown(KeyCode.O)) O_WasPressed();
+        else if (Input.GetKeyDown(KeyCode.W)) W_WasPressed();
+        else if (Input.GetKeyDown(KeyCode.A)) A_WasPressed();
+        else if (Input.GetKeyDown(KeyCode.S)) S_WasPressed();
+        else if (Input.GetKeyDown(KeyCode.D)) D_WasPressed();
+        else if (Input.GetMouseButtonDown(0)) LMB_WasPressed();
+        else if (Input.GetMouseButtonDown(1)) RMB_WasPressed();
+        else if (Input.GetMouseButtonDown(2)) MMB_WasPressed();
+        #endregion
+    }
+
+    public void TestMethod()//this is hooked up with the button in the inspector
+    {
+        WriteDebug("Test Method was invoked");
+    }
+
+    void WriteDebug(string message)
+    {
+        if (_ShowDebugLogs == false) return;
+        Debug.Log(message);
+    }
+    void WriteDebug(string[] messages)
+    {
+        if (_ShowDebugLogs == false) return;
+        foreach (var msg in messages)
+        {
+            Debug.Log(msg);
+        }
+    }
+
+    private void O_WasPressed()
+    {
+        WriteDebug("O_WasPressed");
+    }
+
+    private void P_WasPressed()
+    {
+        WriteDebug("P_WasPressed");
+    }
+
+    private void MMB_WasPressed()
+    {
+        WriteDebug("MMB_WasPressed");
+    }
+
+    private void RMB_WasPressed()
+    {
+        WriteDebug("RMB_WasPressed");
+    }
+
+    private void LMB_WasPressed()
+    {
+        WriteDebug("LMB_WasPressed");
+    }
+
+    private void D_WasPressed()
+    {
+        WriteDebug("D_WasPressed");
+    }
+
+    private void S_WasPressed()
+    {
+        WriteDebug("S_WasPressed");
+    }
+
+    private void A_WasPressed()
+    {
+        WriteDebug("A_WasPressed");
+    }
+
+    private void W_WasPressed()
+    {
+        WriteDebug("W_WasPressed");
+    }
+}
